f86cf0db7cfa4750466cf2ac0de24c7513c620ec
meredith <103847616+merlsey@users.noreply.github.com>
2024-04-06 13:44:18 -0400

    sorry, working code to read data

--- readData.py
+++ readData.py
@@ -1,8 +1,7 @@
-import main
-
-dataSheetNum = input("Enter the selected data sheet number: ")
-print(dataSheetNum)
-
-file = open("sensorData{dataSheetNum}.text")
-print(file.read())
-file.close()
+while True:
+    dataSheetNum = input("Enter the selected data sheet number: ")
+    
+    file_name="sensorData{}.txt".format(dataSheetNum)
+    file = open(file_name)
+    print(file.read())
+    file.close()
